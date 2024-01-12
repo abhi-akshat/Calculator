2507dece1c8a1bef97e6f75f2781d133f92a4001
Abhi Akshat <95142388+abhi-akshat@users.noreply.github.com>
2024-01-12 21:11:48 +0530

    Add files via upload

---
+++ Calculator-iOS/CalculatorApp/CalculatorApp/CalculatorAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppApp.swift
+//  CalculatorApp
+//
+//  Created by Abhi Akshat on 11/01/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
